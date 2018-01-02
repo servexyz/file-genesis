980b3326a2081e6cfefb30724f7cad20ebd8f6cb
alechp <me@alechp.com>
2018-01-02 11:24:34 -0800

    Scaffolded project
    * Added gitignore
    * Removed symlink and config
    * Kept file, directory, content
    
    Going to package everything into types. Will add explanation in readme

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @Author: Alec Hale-Pletka <alechp>
+ * @Date:   2018-01-02T09:33:20-08:00
+ * @Email:  alec@bubblegum.academy
+ * @Last modified by:   alechp
+ * @Last modified time: 2018-01-02T11:21:35-08:00
+ */
+
+const file = require("file");
+const directory = require("directory");
+const content = require("content");
+
+// function writeTemplate() {}
+// function readTemplate() {}
+
+function writeFiles() {
+  /*
+  1. Generate directories
+  2. Populate files within specified directories
+  3. Populate content with specified content
+*/
+}
