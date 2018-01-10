876b31a51d5caf125b7021aa2003a8a62e4fbcb3
alechp <me@alechp.com>
2018-01-10 12:01:39 -0800

    Added tagged literal note

--- templates/sample.template.js
+++ templates/sample.template.js
@@ -3,7 +3,7 @@
  * @Date:   2018-01-09T20:26:20-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-09T20:46:12-08:00
+ * @Last modified time: 2018-01-10T12:01:32-08:00
  */
 
 /*
@@ -14,12 +14,13 @@ Simply being used for testing purposes here.
 Only thing that needs to be replaced for a successful run here is "$Component"...
 If it's replaced with the correct component name, then this works.
 
+TODO: defined taggedLiteral function
 */
 
-export const SAMPLE_TEMPLATE = `
+export const SAMPLE_TEMPLATE = taggedLiteral`
   import React from 'react';
   import PropTypes from 'prop-types';
-  export default class $Component extends React.Component {
+  export default class ${Component} extends React.Component {
     static defaultProps = {
       place: 'holder'
     }
