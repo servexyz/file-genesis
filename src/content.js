55ffa92dcc73a9a9f4f0ef166cae742a699bcf39
alechp <me@alechp.com>
2018-01-11 11:38:54 -0800

    Making progress on content...
    Specifically, figured out the way to interpolate and duplicate original file. Not finished, but making good progress

--- src/content.js
+++ src/content.js
@@ -3,9 +3,61 @@
  * @Date:   2018-01-02T09:34:17-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-02T10:43:09-08:00
+ * @Last modified time: 2018-01-11T11:38:42-08:00
  */
 
-// The purpose of this is to generate content via interpolation
-//Explore:
-// https://unifiedjs.github.io/
+const fs = require("fs-extra");
+const empty = require("is-empty");
+const chalk = require("chalk");
+
+function duplicate(source, destination) {
+  //TODO: Create default destination... Add to paths.js
+  fs
+    .copy(source, destination)
+    .then(() => {
+      log(`Duplicate file created here: ${chalk.green(destination)}`);
+      resolve(true);
+    })
+    .catch(err => {
+      log(`Failed to duplicate file. ${chalk.red(err)}`);
+      reject(false);
+    });
+}
+function getTemplateName(value) {
+  /*
+    1. check first variable
+    2. confirm that it contains template
+    3. reject "false" or resolve "template variable name"
+  */
+  return new Promise((resolve, reject) => {
+    if (value.includes("template.js")) {
+      resolve(value);
+    } else {
+      reject(false);
+    }
+  });
+}
+
+function content(strings, ...values) {
+  let content = "";
+  // replicate file
+  strings.forEach((str, i) => {
+    if (i == 0) {
+      getTemplateName(values[i])
+        .then(template => {
+          //if resolved, means template name was included duplicate
+          duplicate(values[i]);
+        })
+        .catch(err => {
+          log(
+            `content tag failed. Did you remember to include template name as first variable in the template string? \n ${chalk.red(
+              err
+            )}`
+          );
+        });
+    }
+    let strWithNewlines = str.replace(/(\r|\n)/gm, "\n");
+    content += strWithNewlines + (values[i] || "");
+  });
+  return content;
+}
--- templates/sample.template.js
+++ templates/sample.template.js
@@ -3,24 +3,14 @@
  * @Date:   2018-01-09T20:26:20-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-10T12:01:32-08:00
+ * @Last modified time: 2018-01-11T11:15:01-08:00
  */
 
-/*
-NOTE: The component below was copied from https://github.com/servexyz/component-generator-lib/blob/master/src/templates.js
-The purpose of generator-generator is not to manage templates...
-Simply being used for testing purposes here.
-
-Only thing that needs to be replaced for a successful run here is "$Component"...
-If it's replaced with the correct component name, then this works.
-
-TODO: defined taggedLiteral function
-*/
-
-export const SAMPLE_TEMPLATE = taggedLiteral`
+export const SAMPLE_TEMPLATE = `
+  ${"sample.template.js"}
   import React from 'react';
   import PropTypes from 'prop-types';
-  export default class ${Component} extends React.Component {
+  export default class ${component} extends React.Component {
     static defaultProps = {
       place: 'holder'
     }
