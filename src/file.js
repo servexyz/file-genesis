0bd1bf489c0e9ebb9e84e39c1d35ce7f963de2d6
alechp <me@alechp.com>
2018-01-29 16:30:52 -0800

    Tests, API & Docs
    * File is now holding contents of file-new
    * Updated todos and moved unfinished todos to next ver
    * Removed cruft from test

--- src/file.js
+++ src/file.js
@@ -1,83 +1,56 @@
 /**
  * @Author: Alec Hale-Pletka <alechp>
- * @Date:   2018-01-02T09:33:13-08:00
+ * @Date:   2018-01-27T12:24:30-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-27T14:10:17-08:00
+ * @Last modified time: 2018-01-27T16:54:27-08:00
  */
 
-const fs = require("fs-extra");
-const chalk = require("chalk");
-const path = require("path");
-const paths = require("../config/paths.js");
-const empty = require("is-empty");
 const log = console.log;
-const db = paths.DB_CONFIG;
-const { basename, updateDatabase } = require("./helpers.js");
+const chalk = require("chalk");
+const fs = require("fs-extra");
+const { fluent } = require("./helpers.js");
 
-////////////////////////////////////////////////////
-// Create Files
-////////////////////////////////////////////////////
-
-//TODO: Create helper function to simplify fs callback error handling
-function cFilePlain(filename, content) {
-  if (empty(content)) {
-    fs.ensureFile(filename, err => {
-      if (err) {
-        log(`cFileEmpty failed. ${chalk.red(err)}`);
-      } else {
-        log(
-          `Created file${chalk.yellow("<empty>")}:\n ${chalk.blue(filename)}`
-        );
-        updateDatabase(db, filename, "file.empty.last");
-      }
-    });
-  } else {
-    fs.outputFile(filename, content, err => {
-      if (err) {
-        log(`cFilePlain failed. ${chalk.red(err)}`);
-      } else {
-        log(
-          `Created file${chalk.yellow("<plain>")}:\n ${chalk.blue(filename)}`
-        );
-        updateDatabase(db, filename, "file.plain.last");
-      }
-    });
-  }
-}
-
-function cFileSymlink(sourcePath, destinationPath) {
-  fs.ensureSymlink(sourcePath, destinationPath, err => {
-    if (err) {
-      log(`Failed to create symlink. ${chalk.red(err)}`);
-    } else {
-      log(
-        `Created file${chalk.yellow("<symlink>")}:\n ${chalk.blue(
-          destinationPath
-        )}`
-      );
-      updateDatabase(db, destinationPath, "file.symlink.last");
+let File = filepath => {
+  this.filepath = filepath;
+  return {
+    plain: content => {
+      fs.outputFile(this.filepath, content, err => {
+        if (err) {
+          log(
+            `Failed to output ${chalk.yellow("<plain>")} file: ${chalk.yellow(
+              this.filepath
+            )} \n ${chalk.red(err)}`
+          );
+        }
+      });
+    },
+    symlink: origin => {
+      fs.ensureSymlink(origin, this.filepath, err => {
+        if (err) {
+          log(
+            `Failed to output ${chalk.yellow("symlink")} file: ${chalk.yellow(
+              this.filepath
+            )} \n ${chalk.red(err)}`
+          );
+        }
+      });
+    },
+    template: (original, variables) => {
+      //TODO: Play around with async/await
+      const { template } = require("content-genesis");
+      let templateContent = template(original, variables);
+      log(`templateContent: ${chalk.blue(templateContent)}`);
+      fs.outputFile(this.filepath, templateContent, err => {
+        if (err) {
+          log(`Failed to duplicate template. \n ${chalk.red(err)}`);
+          return err;
+        }
+      });
     }
-  });
-}
-function cFileTemplate(filename, content) {
-  //content.template && content.variables
-  const { template } = require("content-genesis");
-  let templateContent = template(content.template, content.variables);
-  fs.outputFile(filename, templateContent, err => {
-    if (err) {
-      log(`cFileTemplate failed. ${chalk.red(err)}`);
-    } else {
-      log(
-        `Created file${chalk.yellow("<template>")}:\n ${chalk.blue(filename)}`
-      );
-      updateDatabase(db, filename, "file.template.last");
-    }
-  });
-}
+  };
+};
 
 module.exports = {
-  cFilePlain,
-  cFileSymlink,
-  cFileTemplate
+  File
 };
--- tests/file.test.js
+++ tests/file.test.js
@@ -3,7 +3,7 @@
  * @Date:   2018-01-04T12:43:32-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-29T14:12:40-08:00
+ * @Last modified time: 2018-01-29T16:29:36-08:00
  */
 
 const log = console.log;
@@ -12,36 +12,12 @@ const chalk = require("chalk");
 const paths = require(path.join(__dirname, "../config/paths.js"));
 const fs = require("fs-extra");
 
-// const db = require("../src/db.js")(paths.T_HISTORY);
-const db = paths.DB_TEST;
-
-// const { createFile } = require(paths.API);
-//
-// //<plain> file variables
 const fName = `foo.bar`;
 const fPath = `${paths.T_SANDBOX}/${fName}`;
 //
-// test("File<plain> created", () => {
-//   createFile(fPath, "empty file", "plain");
-//   db.set("tests.lastPlainFileCreated", fPath).write();
-//   let lastPlainFileCreated = String(db.get("tests.lastPlainFileCreated"));
-//   expect(lastPlainFileCreated).toBe(fPath);
-// });
-//
-// //<symlink> file variables
-// const sName = `.gitignore`;
-// const sOrigin = path.join(__dirname, `../${sName}`);
-// const sDestination = `${paths.T_SANDBOX}/${sName}`;
-//
-// test("File<symlink> created", () => {
-//   createFile(sOrigin, sDestination, "symlink");
-//   db.set("tests.lastSymlinkFileCreated", sDestination).write();
-//   let lastSymlinkFileCreated = String(db.get("tests.lastSymlinkFileCreated"));
-//   expect(lastSymlinkFileCreated).toBe(sDestination);
-// });
-
 const tName = `sample.template.js`;
 const tPath = `${paths.T_SANDBOX}/${tName}`;
+
 //test mostly copied from content-genesis
 let expectedFooUgly = `
  import React from \"react\";
@@ -64,20 +40,12 @@ let expectedFooUgly = `
    }
  }
 `;
-// test("File<template> created", () => {
-//   let content = {
-//     template: require(paths.T_TEMPLATES_SAMPLE),
-//     variables: { component: "Foo" }
-//   };
-//   createFile(tPath, content, "template");
-// });
-//
 
 let filepathPlain = `${paths.T_SANDBOX}/plain.js`;
 let filepathSymlink = `${paths.T_SANDBOX}/symlink.js`;
 let filepathTemplate = `${paths.T_SANDBOX}/template.js`;
 
-test("new-file API", () => {
+test("files<[plain, symlink, template]> created", () => {
   const { File } = require(paths.API);
   File(filepathPlain).plain("foobar");
   File(filepathSymlink).symlink(paths.API);
@@ -86,10 +54,5 @@ test("new-file API", () => {
 });
 
 beforeAll(() => {
-  // let directories = [fPath, sDestination];
-  // for (let d of directories) {
-  //   log(`Removing: ${chalk.yellow(d)}`);
-  //   fs.removeSync(d);
-  // }
   fs.removeSync(paths.T_SANDBOX);
 });
